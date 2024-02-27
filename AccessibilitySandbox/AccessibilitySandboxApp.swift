e7fb2c0122a926eeb52f0eb62653dbc72a3632cc
Uhl Albert <bikrrr@gmail.com>
2024-02-27 14:17:35 -0700

    Initial Commit

---
+++ AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Uhl Albert on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccessibilitySandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AccessibilitySandbox
+//
+//  Created by Uhl Albert on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
